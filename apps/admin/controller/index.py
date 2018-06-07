3b8af3c02791671f6c97a155bc6f73d70e0e19ae
youxingxiang <1365831278@qq.com>
2018-06-07 20:58:37 +0800

    06-07版本

--- apps/admin/controller/index.py
+++ apps/admin/controller/index.py
@@ -1,6 +1,6 @@
 from flask import Blueprint,render_template,session
 from ..model.log import Log
-import psutil
+import psutil,urllib.request,platform
 bp = Blueprint('adminindex',__name__,url_prefix='/admin/index')
 
 @bp.route('/index/')
@@ -8,7 +8,8 @@ def index():
     #系统内存使用情况
     svemm = get_virtual_memory()
     logins = get_login_info()
-    return  render_template('/admin/index/index.html',svemm=svemm,logins=logins)
+    sysinfo = get_sys_info()
+    return  render_template('/admin/index/index.html',svemm=svemm,logins=logins,sysinfo=sysinfo)
 
 #打印本机的内存信息
 def get_virtual_memory():
@@ -21,5 +22,18 @@ def get_login_info():
     登录信息
     :return:
     '''
-    logins = Log.query.filter(Log.log_type=="login").order_by(Log.id.desc()).limit(8).offset(0).all()
+    logins = Log.query.filter(Log.log_type=="login").order_by(Log.id.desc()).limit(9).offset(0).all()
     return logins
+
+
+def get_sys_info():
+    '''
+    获取系统信息
+    :return:
+    '''
+    sysinfo = {}
+    sysinfo['系统cpu位数'] = platform.processor()
+    sysinfo['系统信息'] = platform.system()
+    sysinfo['操作系统类型'] = platform.dist()[0]
+    sysinfo['系统主机名'] = platform.node()
+    return sysinfo
